74b42cf9dda755f31f6998309b20dd860663ebe6
songmikim <ksm0200@gmail.com>
2025-08-19 18:23:03 +0900

    .

--- src/app/member/social/_services/SocialApi.ts
+++ src/app/member/social/_services/SocialApi.ts
@@ -1,9 +1,19 @@
 export type ProfileType = {
-    id: number | string
+  id: number | string
 }
 
+/**
+ * authorization code
+ */
 export default interface SocialApi {
-  channel: 'KAKAO' | 'NAVER'
   getToken: (code: string) => string
   getProfile: (token: string) => ProfileType
+  
+  /**
+   * 인증 서버에서 client_id를 통해서 authorization code를 발급받을 수 있는 URL 생성
+   * authorization code : access token을 발급 받기 위한 인증 코드 
+   * @param redirectUrl : 없으면 메인 페이지, 있으면 로그인 처리 후  지정된 URL로 이동
+   * @returns 
+   */
+  getUrl: (redirectUrl: string) => string
 }
