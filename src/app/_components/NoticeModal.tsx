34d972b4c651019dd1feb2a4fb5c443340baf0eb
songmikim <50817231+songmikim@users.noreply.github.com>
2025-08-28 23:56:14 +0900

    .

--- src/app/_components/NoticeModal.tsx
+++ src/app/_components/NoticeModal.tsx
@@ -5,24 +5,71 @@ import styled from 'styled-components'
 import { format } from 'date-fns'
 import LayerPopup from '../_global/components/LayerPopup'
 import type { BoardDataType } from '@/app/board/_types/BoardType'
+import guideImg from '@/app/_global/assets/images/guide.png'
+import fontsize from '../_global/styles/fontsize'
+
+const Title = styled.h1`
+  font-size: ${fontsize.big};
+  font-weight: bold;
+  text-align: center;
+  margin: 10px;
+`
 
 const List = styled.ul`
-  margin-top: 20px;
-  li + li {
-    margin-top: 10px;
-  }
+  border-top: 1px solid #ccc;
+
   li {
     display: flex;
     justify-content: space-between;
+    border-bottom: 1px solid #ccc;
+    padding:6px;
   }
+    
+  .center {
+    text-align: center;
+    display: block;
+  }  
+
   a {
     text-decoration: none;
     color: #333;
   }
   time {
     color: #666;
-    font-size: 0.875rem;
+    font-size: ${fontsize.small};
   }
+`
+
+const ImageWrapper = styled.div`
+  position: relative;
+  display: inline-block; 
+  width: 500px;
+  z-index:-1;
+`
+
+const OverlayTexts = styled.div`
+  position: absolute;
+  width: 220px;
+  top: 50px; 
+  right: 10px; 
+  text-align: left;
+  letter-spacing: -1px;
+
+  h2 {
+    font-size: 2.3rem;
+    margin: 0;
+    font-weight: bold;
+    color: #222;
+    text-align: left;
+    padding:0;
+  }
+
+  p {
+    font-size: ${fontsize.medium}; 
+    margin: 4px 0 0;
+    color: #555;
+  }
+
 `
 
 type Props = {
@@ -34,10 +81,19 @@ const NoticeModal = ({ items }: Props) => {
 
   return (
     <LayerPopup isOpen={open} onClose={() => setOpen(false)} width={500}>
-      <Image src="/assets/images/guide.png" alt="guide" width={480} height={240} />
+      <ImageWrapper>
+        <Image src={guideImg} alt="guide" width={500} />
+        {/* 공지 타이틀 */}
+      <Title>공지사항</Title>
+        <OverlayTexts>
+          <h2>단순한 배경에서<br/>촬영해주세요</h2>
+          <p>배경이 복잡하거나 물건이 많이 보이는 경우에는 정확도가 떨어질 수 있으니, 가능하다면 깔끔한 배경 위에서 촬영해 주시기 바랍니다.</p>
+        </OverlayTexts>
+      </ImageWrapper>
+
       <List>
         {items && items.length > 0 ? (
-          items.map((item) => (
+          items.slice(0, 5).map((item) => (
             <li key={item.seq}>
               <a href={`/board/view/${item.seq}`}>{item.subject}</a>
               {item.createdAt && (
@@ -48,7 +104,7 @@ const NoticeModal = ({ items }: Props) => {
             </li>
           ))
         ) : (
-          <li>공지글이 없습니다.</li>
+          <li className='center'>공지글이 없습니다.</li>
         )}
       </List>
     </LayerPopup>
