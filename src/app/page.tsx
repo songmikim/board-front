ea46df77e95acbc1df75d5788d5b938a504cf566
songmikim <50817231+songmikim@users.noreply.github.com>
2025-08-28 17:55:03 +0900

    feat: display notice modal on main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
+import NoticeModal from './_components/NoticeModal'
+import { getList } from '@/app/board/_services/boardData'
 
-export default function MainPage() {
-
-  return <></>
+export default async function MainPage() {
+  const { items } = await getList('notice', { limit: 5 })
+  return <NoticeModal items={items} />
 }
